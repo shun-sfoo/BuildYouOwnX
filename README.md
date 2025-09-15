e3a332f016f7201db07d548190ed8f41e69af6eb
shun-sfoo <ganymede0915@gmail.com>
2025-09-15 21:35:42 +0800

    lispy: Editing Input

---
+++ README.md
@@ -0,0 +1,10 @@
+# Build You Own X
+
+## Lispy
+
+In order to use `editline` functions, and arrow key.
+need install package
+```bash
+sudo apt install libedit-dev
+```
+
--- lispy/main.c
+++ lispy/main.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h> /* free */
 
-/* Declare a buffer for user input size 2048 */
-static char input[2048];
+/* If we are compiling on Windows compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char *readline(char *prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char *cpy = malloc(strlen(buffer) + 1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy) - 1] = '\0';
+  return cpy;
+}
+
+/* Fake add_history function */
+void add_history(*char *unused) {}
+
+#elif defined(__LINUX__)
+#include <editline/history.h>
+#include <editline/readline.h>
+
+#elif defined(__APPLE__)
+#include <editline/readline.h>
+#endif
 
 int main(int argc, char **argv) {
   /* Print Version and Exit Information */
@@ -11,14 +36,17 @@ int main(int argc, char **argv) {
   /* In a nerver ending loop */
   while (1) {
 
-    /* Output our prompt */
-    fputs("lispy> ", stdout);
+    /* Output our prompt and get input */
+    char *input = readline("lispy> ");
 
-    /* Read a line of user input of maximum size 2048 */
-    fgets(input, 2048, stdin);
+    /* Add input to history*/
+    add_history(input);
 
     /* Echo input back to user */
-    printf("No you're a %s", input);
+    printf("No you're a %s\n", input);
+
+    /* Free retrieved input */
+    free(input);
   }
 
   return 0;
